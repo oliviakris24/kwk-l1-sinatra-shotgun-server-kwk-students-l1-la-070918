dc402fb7ab11ed7606bd98dd7782d46b67b0c2e1
oliviakris24 <okfishy@gmail.com>
2018-07-17 18:09:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "hey guys my name is olivia "
+    "hey guys my name is olivia and i have a bird "
   end
 
 end
